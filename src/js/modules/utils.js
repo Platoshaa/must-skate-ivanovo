760203669e72a8fae5e3dc9961b85a527a9b08ea
Platoshaa <pkolesnikov1239@gmail.com>
2024-04-05 17:32:58 +0300

    scripts

--- src/js/modules/utils.js
+++ src/js/modules/utils.js
@@ -35,10 +35,16 @@ export function isWebp() {
     webP.src =
       "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
   }
-
-  // Добавление класса _webp или _no-webp для HTML
   testWebp(function (support) {
     let className = support === true ? "webp" : "no-webp";
     document.documentElement.classList.add(className);
   });
+}
+export function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,57 +1,21 @@
-import { isMobile, isWebp } from "./modules/utils.js";
+import { isMobile, isWebp, getRandomColor } from "./modules/utils.js";
 import { gsap } from "gsap/all.js";
 import { ScrollTrigger } from "gsap/ScrollTrigger.js";
 import LocomotiveScroll from "locomotive-scroll";
 import Swiper from "swiper/bundle";
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
-// import styles bundle
 import "swiper/css/bundle";
-isWebp();
-gsap.registerPlugin(ScrollTrigger);
-Fancybox.bind("[data-fancybox]", {});
+
 if (isMobile.any() || document.body.clientWidth <= 768) {
   document.body.className = "mobile";
   document.body.innerHTML = `<div>SHAME on me! i haven't done a mobile version. Sorry but the main thing is horizontalal scrolling, which i would probably turn off on mobiles anyway</div> <a href="" ><svg>
   <use xlink:href="images/icons/icons.svg#logo"></use>
 </svg></a>`;
 } else {
-  function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
-  let bg = document.querySelector(".autoplay-progress");
-  const swiper = new Swiper(".front-slider", {
-    speed: 2400,
-    direction: "vertical",
-    autoplay: {
-      delay: 4500,
-    },
-    loop: true,
-    watchSlidesProgress: true,
-    parallax: {
-      enable: true,
-    },
-    on: {
-      slideChange() {
-        bg.style.transition = "opacity 1s";
-        bg.style.background = getRandomColor();
-      },
-      autoplayTimeLeft(s, time, progress) {
-        if (progress < 0.3) {
-          bg.style.transition = "opacity 0s";
-          bg.style.opacity = ` ${progress}`;
-        } else {
-          bg.style.opacity = ".3";
-        }
-      },
-    },
-  });
+  isWebp();
+  gsap.registerPlugin(ScrollTrigger);
+  Fancybox.bind("[data-fancybox]", {});
 
   const scroller = document.querySelector("[data-scroll-container]");
   const locoScroll = new LocomotiveScroll({
@@ -59,24 +23,6 @@ if (isMobile.any() || document.body.clientWidth <= 768) {
     smooth: true,
     repeat: true,
     direction: "horizontal",
-    smartphone: {
-      smooth: true,
-      direction: "horizontal",
-    },
-    tablet: {
-      smooth: true,
-      direction: "horizontal",
-    },
-  });
-  locoScroll.on("scroll", () => {
-    ScrollTrigger.update();
-    const secIn = document.querySelectorAll(".section.is-inview");
-    const last = secIn[secIn.length - 1];
-    const header = document.querySelector(".header");
-    const color = window
-      .getComputedStyle(last, null)
-      .getPropertyValue("background-color");
-    header.style.setProperty("--main-color", color);
   });
   ScrollTrigger.scrollerProxy(scroller, {
     scrollLeft(value) {
@@ -102,67 +48,106 @@ if (isMobile.any() || document.body.clientWidth <= 768) {
   ScrollTrigger.defaults({
     scroller: scroller,
   });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+
+  /////////////////////////////////////
+  ////Blog section vertical scroll/////
+  /////////////////////////////////////
   gsap.to(".anim-wrap", {
     scrollTrigger: {
       trigger: ".blog__list",
       start: () => {
         return "left left";
       },
-      end: () =>
-        "+=" + document.querySelector(".anim-wrap").scrollHeight,
+      end: () => "+=" + document.querySelector(".anim-wrap").scrollHeight,
       pin: true,
       pinSpacing: true,
-      // anticipatePin: 1,
       scrub: true,
       horizontal: true,
       invalidateOnRefresh: true,
-      // markers: true,
-      // onEnter: ({ progress, direction, isActive }) => {
-      //   console.log(progress, direction, isActive);
-      // },
     },
     y: () => {
       return -(
-        document.querySelector(".anim-wrap").scrollHeight -
-        window.innerHeight
+        document.querySelector(".anim-wrap").scrollHeight - window.innerHeight
       );
     },
-    // ease: "none",
   });
-  const s = document.querySelectorAll(".section");
-  let width = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i].classList.contains("blog")) {
-      break;
-    } else {
-      width += Number(s[i].offsetWidth);
+  /////////////////////////////
+  ////Blog items is inview/////
+  /////////////////////////////
+  function widthTillSection(sectionName) {
+    const sections = document.querySelectorAll(".section");
+    let sectionsWidth = 0;
+    for (let i = 0; i < sections.length; i++) {
+      if (sections[i].classList.contains(sectionName)) {
+        break;
+      } else {
+        sectionsWidth += Number(sections[i].offsetWidth);
+      }
     }
+    return sectionsWidth;
   }
-
-  const ar = document.querySelectorAll(".blog-list__item");
-  ar.forEach((e) => {
-    const startPoint = width + +e.getBoundingClientRect().y / 5;
-    gsap.to(e, {
+  const blogItems = document.querySelectorAll(".blog-list__item");
+  blogItems.forEach((item) => {
+    const startPoint =
+      widthTillSection("blog") + +item.getBoundingClientRect().y / 5;
+    gsap.to(item, {
       scrollTrigger: {
-        trigger: e,
+        trigger: item,
         start: () => {
           return startPoint + " top";
         },
         end: () => {
-          return startPoint + e.scrollHeight * 5 + " center";
+          return startPoint + item.scrollHeight * 5 + " center";
         },
-        toggleClass: "red",
+        toggleClass: "is-inview",
         scrub: 3,
         duration: 0.2,
         ease: "power1.inOut",
       },
     });
   });
-
-  ScrollTrigger.addEventListener("refresh", () =>
-    locoScroll.update()
-  );
-  ScrollTrigger.refresh();
+  locoScroll.on("scroll", () => {
+    ScrollTrigger.update();
+    const secIn = document.querySelectorAll(".section.is-inview");
+    const last = secIn[secIn.length - 1];
+    const header = document.querySelector(".header");
+    const color = window
+      .getComputedStyle(last, null)
+      .getPropertyValue("background-color");
+    header.style.setProperty("--main-color", color);
+  });
+  ///////////////
+  ////Slider////
+  /////////////
+  const swiperProgressBackground = document.querySelector(".autoplay-progress");
+  const swiper = new Swiper(".front-slider", {
+    speed: 2400,
+    direction: "vertical",
+    autoplay: {
+      delay: 4500,
+    },
+    loop: true,
+    watchSlidesProgress: true,
+    parallax: {
+      enable: true,
+    },
+    on: {
+      slideChange() {
+        swiperProgressBackground.style.transition = "opacity 1s";
+        swiperProgressBackground.style.background = getRandomColor();
+      },
+      autoplayTimeLeft(s, time, progress) {
+        if (progress < 0.3) {
+          swiperProgressBackground.style.transition = "opacity 0s";
+          swiperProgressBackground.style.opacity = ` ${progress}`;
+        } else {
+          swiperProgressBackground.style.opacity = ".3";
+        }
+      },
+    },
+  });
   window.addEventListener("load", function () {
     document.querySelector(".loader").classList.add("active");
   });
